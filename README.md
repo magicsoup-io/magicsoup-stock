d73e0f6576f54b11fd95298fd7f867f399bb61de
Simon Franzen <simon@zauberware.com>
2019-01-24 15:46:17 +0100

    Initial commit. Adding package.json, .eslintrc, .gitignore, .babelrc and an empty Readme.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# MagicSoup.io - Stock
+
+Base component library for all MagicSoup components.
+
+## Background
+
+Inspired by rebass we created our own component library.
+
+tbd
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|dist|coverage|lib|public)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  externals: {
+    react: 'commonjs react',
+  },
+}
