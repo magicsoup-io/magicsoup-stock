3f5868edeea9ea79dd9c84911bf04bb006af1bb1
Simon Franzen <simon@zauberware.com>
2019-01-31 21:30:36 +0100

    smaller bundlesize + add npmignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,67 @@
-const path = require('path')
+const webpack = require('webpack');
+const pkg = require('./package.json');
+const path = require('path');
+const libraryName= pkg.name;
 
 module.exports = {
-  entry: './src/index.js',
+  entry: path.join(__dirname, "./src/index.js"),
   mode: 'production',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.join(__dirname, './dist'),
     filename: 'index.js',
-    libraryTarget: 'commonjs2',
+    library: libraryName,
+    libraryTarget: 'umd',
+    publicPath: '/dist/',
+    umdNamedDefine: true,
+  },
+  node: {
+    net: 'empty',
+    tls: 'empty',
+    dns: 'empty'
   },
   module: {
-    rules: [
+    rules : [
       {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
-        exclude: /(node_modules|bower_components|dist|coverage|lib|public)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-    ],
+          test: /\.(js|jsx)$/,
+          use: ["babel-loader"],
+          include: path.resolve(__dirname, "src"),
+          exclude: [/node_modules/, /coverage/, /lib/, /public/],
+      }
+    ]
+  },
+  resolve: { 
+      alias: { 
+          'react': path.resolve(__dirname, './node_modules/react') ,
+          'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+          'styled-system': path.resolve(__dirname, './node_modules/styled-system'),
+          'styled-components': path.resolve(__dirname, './node_modules/styled-components')
+      } 
   },
   externals: {
-    react: 'commonjs react',
-  },
-}
+      // Don't bundle react, react-dom, styled-system or styled-components
+      react: {
+          commonjs: "react",
+          commonjs2: "react",
+          amd: "React",
+          root: "React"
+      },
+      "react-dom": {
+          commonjs: "react-dom",
+          commonjs2: "react-dom",
+          amd: "ReactDOM",
+          root: "ReactDOM"
+      },
+      "styled-system": {
+          commonjs: "styled-system",
+          commonjs2: "styled-system",
+          amd: "styled-system",
+          root: "styled-system"
+      },
+      "styled-components": {
+          commonjs: "styled-components",
+          commonjs2: "styled-components",
+          amd: "styled-components",
+          root: "styled-components"
+      }
+  }
+};
