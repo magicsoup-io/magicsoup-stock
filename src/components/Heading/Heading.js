309251c66aa9078289ff830c9dd621ee07aac413
Simon Franzen <simon@zauberware.com>
2019-02-05 23:35:46 +0100

    remove default prop for headlings. Add variants for Headings

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -1,18 +1,25 @@
 import styled from 'styled-components'
+import { variant } from 'styled-system'
+import PropTypes from 'prop-types'
 
 import Text from '../Text'
 
 const themed = key => props => props.theme[key]
 
+const headings = variant({ key: 'headings' })
+
 const Heading = styled(Text)`
+  ${headings}
   ${themed('Heading')}
 `
 /** @component */
 export default Heading
 
+Image.propTypes = {
+  ...headings.propTypes,
+  as: PropTypes.string,
+}
+
 Heading.defaultProps = {
   as: 'h2',
-  m: 0,
-  fontSize: 4,
-  fontWeight: 'bold',
 }
