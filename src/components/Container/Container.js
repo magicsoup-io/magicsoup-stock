1fbcd5db3f9baccb5124c969d61bf07801634687
Simon Franzen <simon@zauberware.com>
2019-02-07 14:38:19 +0100

    clean up Container components -> apply styled from theme.js last like the other components

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,20 +1,23 @@
-// styled-components based extension
 import styled from 'styled-components'
 import { maxWidth } from 'styled-system'
 import Box from '../Box'
 
 const themed = key => props => props.theme[key]
 
-const Container = styled(Box)(
-  themed('Container'),
-  maxWidth,
-)
+const Container = styled(Box)`
+  ${maxWidth}
+  ${themed('Container')}
+`
+
+/** @component */
+export default Container
+
+Container.propTypes = {
+  ...maxWidth.propTypes,
+}
 
 Container.defaultProps = {
   mx: 'auto',
   px: 2,
   maxWidth: 1180,
 }
-
-/** @component */
-export default Container
